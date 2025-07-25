5460d1b655728a46dfcf3baf964f0d87f940b39d
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-07-25 11:11:16 +0300

    feat: implement secure login with access/refresh token and cookie support

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from "express";
 import User from "../models/user.model";
+import { comparePassword } from "../utils/bcrypt";
+import { signAccessToken, signRefreshToken } from "../utils/jwt";
 
 export const register = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
@@ -27,4 +29,45 @@ export const register = async (req: Request, res: Response) => {
   } catch (error: any) {
     return res.status(500).json({ success: false, message: error?.message });
   }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user)
+    return res.status(404).json({ success: false, message: "User not found" });
+
+  const isMatch = await comparePassword(password, user.password);
+  if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
+
+  const accessToken = signAccessToken({
+    id: user._id.toString(),
+    role: user.role,
+  });
+
+  const refreshToken = signRefreshToken({
+    id: user._id.toString(),
+    role: user.role,
+  });
+
+  user.refreshTokens.push(refreshToken);
+  await user.save();
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+  return res.json({
+    accessToken,
+    success: true,
+    message: "Logged in successfully",
+  });
 };
