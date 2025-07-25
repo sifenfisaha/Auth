9c6d764a39cf9c33b6c630711ad59eda1ec808fc
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-07-25 11:32:00 +0300

    chore: install and configure cookie-parser middleware for handling cookies

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDb from "./configs/mongodb.config";
 import authRouter from "./routes/auth.routes";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send({ mes: "working" });
