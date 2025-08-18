422993e94bbafcaf4068343bc0cdc37545cddf89
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-08-18 16:15:00 +0300

    feat(types): extend Express Request with uses and toke properties

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -3,5 +3,15 @@ import "express-serve-static-core";
 declare module "express-serve-static-core" {
   interface Request {
     data?: Record<string, any>;
+
+    user?: {
+      id: string;
+      email?: string;
+      username?: string;
+      roles?: string[];
+      isAdmin?: boolean;
+    };
+
+    token?: string;
   }
 }
