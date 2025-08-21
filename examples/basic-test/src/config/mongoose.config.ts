b4e9b066ddef94c46d5fb06314f379d6cc27dd1a
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-08-21 11:40:47 +0300

    feat: add examples folder for local package testing
    - Created an  folder at the root for testing the auth and mongoose adapters.
    - Set up a basic Express project inside  to simulate usage of the package before publishing.
    - Allows testing changes locally without publishing the package to npm.

---
+++ examples/basic-test/src/config/mongoose.config.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDb = async (cb?: () => void) => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+    console.log(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Successfully connected to MongoDB");
+    if (cb) cb();
+  } catch (error: any) {
+    console.error("MongoDB connection error:", error.message);
+  }
+};
+
+export default connectDb;
