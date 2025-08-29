5d46ffeb32a7134b0b52a303ff9133468dfa48f9
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-08-29 14:35:24 +0300

    feat(api): implement axios client, tokenManager, and auth endpoints

---
+++ packages/auth-react/src/api/endpoint.ts
@@ -0,0 +1,11 @@
+export const endpoints = {
+  login: "/auth/login",
+  register: "/auth/register",
+  logout: "/auth/logout",
+  refresh: "/auth/refresh-token",
+  me: "/auth/me",
+  requestVerification: "/auth/request-verification",
+  confirmVerification: "/auth/confirm-verification",
+  forgotPassword: "/auth/forgot-password",
+  resetPassword: "/auth/reset-password",
+};
